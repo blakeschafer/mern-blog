b8f2564886080a77a31dceef859d901fe41b54cf
Blake-Schafer <102622486+Blake-Schafer@users.noreply.github.com>
2023-09-05 18:43:40 -0700

    fix Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,25 +11,33 @@ const navItemInfo = [
 ]
 
 const NavItem = ({ item }) => {
+const[dropdown, setDropdown] = useState(false);
+
+const togggleDropdownHandler = () => {
+    setDropdown((curState) => {
+        return !curState;
+    });
+};
+
 return (
     <li className='relative group'>
-        *
+        {/* * */}
         {item.type === "link" ? ( <> <a href="/" className='px-4 py-2 group-hover:text-'> 
             {item.name} 
         
         </a>
-        <span className='text-yellow-500 absolute transition-all duration-500 font-bold right-0 top-0 group-hover:opacity-100'>
+        <span className='cursor-pointer text-yellow-500 absolute transition-all duration-500 font-bold right-0 top-0 group-hover:right-[90%] opacity-0 group-hover:opacity-100'>
         
         </span></> ) : (
-        <>
+        <div className="flex flex-col items-center">
         
-        <a href="/" className='px-4 py-2 items-center-flex'> 
+        <button className='px-4 py-2 items-center-flex' onClick={togggleDropdownHandler}> 
         
             {item.name}
             
-        </a>
-        <div className="hidden transition-all duration-500 pt-4 absolute bottom-0 right-0 transform translate-y-full group-hover:block w-max">
-            <ul className="flex flex-col shadow-lg rounded-lg overflow-hidden">
+        </button>
+        <div className={`${dropdown ? "block" : "hidden"} lg:hidden transition-all duration-500 pt-4 lg:absolute lg:bottom-0 lg:right-0 lg:transform lg:translate-y-full lg:group-hover:block w-max`}>
+            <ul className="bg-dark-hard lg:bg-trasnparent text-centerflex flex-col shadow-lg rounded-lg overflow-hidden">
                 {item.items.map((page) => (
                     <a href="/" className="hover:bg-dark-hard hover:text-white px-4 py-2 text-dark lg:text-dark-hard">
                     {page}
@@ -39,7 +47,7 @@ return (
             </ul>
             
         </div>
-        </>
+        </div>
         )}
     </li>
     );
